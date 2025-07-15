36d53a923b7c52613f7e00edb621c6736a446910
Asgeir Jacobsen <asgeirjacobsen@gmail.com>
2025-07-15 13:41:34 +0200

    updated link in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer>
       <p>Funnet feil? Gi meg beskjed -></p>
-      <a href="UGYLDIG_LINK">
+      <a href="https://github.com/AJC0520/naar-stenger-olsalget">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png"
           alt="github logo"
